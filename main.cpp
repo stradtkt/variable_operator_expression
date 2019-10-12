bbf9b4006042938f33d36d6b5cb0df9fb25d32cd
Kevin <stradtkt22@gmail.com>
2019-10-12 18:41:42 -0400

    Added some functions to the program

--- main.cpp
+++ main.cpp
@@ -19,6 +19,19 @@ void simple_interest(int p, int r, int t) {
     cout << (p*r*t)/100 << endl;
 }
 
+double area_of_circle(double r) {
+    double PI = 3.14;
+    return ((PI)*(r)*(r));
+}
+
+void positive_or_negative(int num) {
+    cout << ((num > 0) ? "Positive" : "Negative") << endl;
+}
+
+void even_or_odd(int num) {
+    cout << ((num % 2 == 0) ? "Even" : "Odd") << endl;
+}
+
 int main() {
     sum(2,4);
     f_to_c(102);
@@ -30,5 +43,13 @@ int main() {
     cout << "Enter a character for the ASCII Value of it: ";
     cin >> input;
     cout << "The ASCII value for " << input << " is " << int(input) << endl;
+
+    cout << "Area of a circle is " << area_of_circle(2.14) << endl;
+
+    positive_or_negative(-11);
+    positive_or_negative(10);
+
+    even_or_odd(10);
+    even_or_odd(11);
     return 0;
 }
